ed34417f750f60e08383d8cb02135b9fc8b2ce70
Jessé Lessa <lessaadv7@gmail.com>
2026-09-17 08:21:36 -0300

    Implementação de item na classe Treinador

--- src/main/java/Treinador.java
+++ src/main/java/Treinador.java
@@ -1,8 +1,10 @@
 public class Treinador {
     private String nome;
     private Pokemon pokemon;
+    private int itensUsados;
 
     public Treinador(String nome, Pokemon pokemon) {
+        this.itensUsados = 0;
         this.nome = nome;
         this.pokemon = pokemon;
     }
@@ -13,4 +15,44 @@ public class Treinador {
         return pokemon;
     }
 
+    public int getItensUsados() {
+        return itensUsados;
+    }
+
+    public boolean podeUsarItem(){
+        return itensUsados < 2;
+    }
+
+    public void usarItem(){
+        if(podeUsarItem()){
+            itensUsados++;
+        }
+    }
+
+    public void usarItem(Item item) {
+
+        if (!podeUsarItem()) {
+            System.out.println("Você já usou os 2 itens permitidos.");
+            return;
+        }
+
+        switch (item) {
+
+            case POTION:
+                pokemon.setHp(pokemon.getHp() + 20);
+                break;
+
+            case SUPER_POTION:
+                pokemon.setHp(pokemon.getHp() + 40);
+                break;
+
+            case ANTIDOTO:
+                if (pokemon.getStatus() == Status.ENVENENADO) {
+                    pokemon.setStatus(Status.NORMAL);
+                }
+                break;
+        }
+
+        itensUsados++;
+    }
 }
